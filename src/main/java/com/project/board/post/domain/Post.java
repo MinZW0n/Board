fb3d3b1edec8299478a4af30183516eb3ce03f4c
민지원 <mjiwon9971@naver.com>
2024-01-10 22:21:00 +0900

    add post entity

---
+++ src/main/java/com/project/board/post/domain/Post.java
@@ -0,0 +1,41 @@
+package com.project.board.post.domain;
+
+import com.project.board.board.domain.Board;
+import com.project.board.comment.domain.Comment;
+import com.project.board.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "comment_id")
+    private List<Comment> comment;
+}
