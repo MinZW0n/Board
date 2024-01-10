752dea1a701ae73859ff77c924afb4d8be688215
민지원 <mjiwon9971@naver.com>
2024-01-10 22:20:39 +0900

    add comment entity

---
+++ src/main/java/com/project/board/comment/domain/Comment.java
@@ -0,0 +1,32 @@
+package com.project.board.comment.domain;
+
+import com.project.board.post.domain.Post;
+import com.project.board.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "comment")
+    private Post post;
+}
